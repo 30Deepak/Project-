746d08d12f8fc3d17a1262868f26da42fd48f441
30Deepak <131438866+30Deepak@users.noreply.github.com>
2023-04-21 17:28:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Mat;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) 
+	{
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	}
+}
